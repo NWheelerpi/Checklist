4215a86583106c90eee740dfa40027d58eaf5b05
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2026-08-01 16:29:16 +0100

    h

--- test.py
+++ test.py
@@ -26,5 +26,6 @@ def layout(Tab,checklist,checklist_key,tab_num,Image):
         outer_cols = st.columns([1,2])
 
         with outer_cols[1]:
-            xylist=streamlit_image_coordinates(Image,width='stretch',key='Image'+str(tab_num))
+            xy=streamlit_image_coordinates(Image,width='stretch',key='Image'+str(tab_num))
+            st.write(xy)
     return
