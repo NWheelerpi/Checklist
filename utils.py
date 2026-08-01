ef6e558a2eba1e0a16081656749f23f25127ff98
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2026-08-01 17:04:04 +0100

    g

--- utils.py
+++ utils.py
@@ -4,21 +4,23 @@ def nearest_location():
     print('Nearest Location')
     return
 
+def selected(string,tabnum,key):
+    st.markdown(string)
+    return
 def selected_image(checklist_key):
     'Function that adds selected actions from action image'
     # Calculate nearest 'action'
     # Correct values for reshaped grid 
     value=st.session_state.xylist[checklist_key]
     st.markdown(value)
-    '''
-    xval*=860/self.xval
-    yval*=640/self.yval
+    xval=value['x']*860/value['width']
+    yval=value['y']*640/value['height']
     dist_dict={}
-    for i in self.action_dict:
-        Distance=(xval-self.action_dict[i][1])**2+(yval-self.action_dict[i][2])**2
+    for i in st.session_state.action_dict:
+        Distance=(xval-st.session_state.action_dict[i][1])**2+(yval-st.session_state.action_dict[i][2])**2
         dist_dict[Distance]=i
     # Check close enough to a switch 
-    #print(dist_dict[sorted(dist_dict)[0]],self.action_dict[dist_dict[sorted(dist_dict)[0]]])
+    #print(dist_dict[sorted(dist_dict)[0]],st.session_state.action_dict[dist_dict[sorted(dist_dict)[0]]])
     if dist_dict[sorted(dist_dict)[0]] <=100:   
         action_index=dist_dict[sorted(dist_dict)[0]]
     else:
@@ -27,12 +29,12 @@ def selected_image(checklist_key):
     # Create correct string
     action_string=None
     if action_index!=None:
-        if string=='on' and type(self.action_dict[action_index][3])!=float and self.action_dict[action_index][3]!='NONE':
-            action_string=self.action_dict[action_index][3]
-        elif string=='off' and type(self.action_dict[action_index][4])!=float and self.action_dict[action_index][4]!='NONE':
-            action_string=self.action_dict[action_index][4]
-        elif string=='check' and type(self.action_dict[action_index][5])!=float and self.action_dict[action_index][5]!='NONE':
-            action_string=self.action_dict[action_index][5]
+        if string=='on' and type(st.session_state.action_dict[action_index][3])!=float and st.session_state.action_dict[action_index][3]!='NONE':
+            action_string=st.session_state.action_dict[action_index][3]
+        elif string=='off' and type(st.session_state.action_dict[action_index][4])!=float and st.session_state.action_dict[action_index][4]!='NONE':
+            action_string=st.session_state.action_dict[action_index][4]
+        elif string=='check' and type(st.session_state.action_dict[action_index][5])!=float and st.session_state.action_dict[action_index][5]!='NONE':
+            action_string=st.session_state.action_dict[action_index][5]
     else:
         pass
     
@@ -40,7 +42,7 @@ def selected_image(checklist_key):
     
     # Add/remove from text
     if action_string!=None:
-        self.selected(action_string,tabnum,Key)
+        selected(action_string,tabnum,Key)
     '''
 
 
