81e9bef4ec61b2f2c6959f8fcc98733a0263b44f
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2026-08-01 16:16:13 +0100

    j

--- plog.py
+++ plog.py
@@ -31,4 +31,4 @@ st.markdown(st.session_state.Keys)
 Tabs=st.tabs([str(i+1) for i in range(len(st.session_state.Keys))])
 
 for j,i in enumerate(st.session_state.num_checklist_list):
-    layout(Tabs[j],st.session_state.emergency_dict[st.session_state.Keys[j]],st.session_state.Keys[j],j,Cockpit.png)
+    layout(Tabs[j],st.session_state.emergency_dict[st.session_state.Keys[j]],st.session_state.Keys[j],j,'Cockpit.png')
