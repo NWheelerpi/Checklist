cfd8a2e5e19f39032a30f616f2e57401b139a1c3
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2026-08-01 15:33:19 +0100

    g

--- plog.py
+++ plog.py
@@ -2,6 +2,7 @@ import pandas as pd
 import streamlit as st
 from utils import *
 from test import *
+import random
 ### Extract Emergencies
 
 book= 'Tutor_Emergencies.xlsx'
@@ -13,4 +14,11 @@ for i in range(len(action_image)):
 
 st.session_state.Keys=[i for i in st.session_state.emergency_dict.keys()]
 
+##### Randomise Emergencies as required
+num_checklist_list=list(np.arange(0,len(st.session_state.Keys),1))
+random.shuffle(num_checklist_list)
+    
 st.markdown(st.session_state.Keys)
+
+for j,i in enumerate(num_checklist_list):
+    layout(self.windowlist[j*2],st.session_state.emergency_dict[st.session_state.Keys[num_checklist_list[j]]],st.session_state.Keys[num_checklist_list[j]],j*2)
