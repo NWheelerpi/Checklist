9e2f5f6fa3f05b5cf1c74f17db3b282b65fd3ae2
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2026-08-01 16:03:49 +0100

    g

--- test.py
+++ test.py
@@ -14,7 +14,8 @@ def layout(Tab,checklist,checklist_key,tab_num):
     actions=checklist.T.values.tolist()[0]
     conditionals=checklist.T.values.tolist()[1]
 
-    title=[i for i in checklist_key.splitlines('_')]
+    title=[i for i in checklist_key.split('_')]
+
     with Tab:
         st.header(title)
     return
