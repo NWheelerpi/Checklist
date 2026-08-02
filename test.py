3bac1f9963b0526160f5db9e374c3e45690e9f02
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2026-08-02 15:13:35 +0100

    g

--- test.py
+++ test.py
@@ -26,6 +26,7 @@ def layout(Tab,checklist,checklist_key,tab_num,Image):
         outer_cols = st.columns([1,2])
 
         with outer_cols[1]:
+            click_type=st.radio('Click Type',['On','Off','Check'],key='Radio'+str(tab_num),label_visibility='collapsed',horizontal=True)
             xy=streamlit_image_coordinates(Image,use_column_width=True,key='Image'+str(tab_num),cursor='crosshair')
             st.session_state.xylist[checklist_key]=xy
     return
