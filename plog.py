96e3400c406adcb8b2736bf2ef6dff004cbcb34e
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2026-08-02 16:12:26 +0100

    j

--- plog.py
+++ plog.py
@@ -26,11 +26,16 @@ action_image=pd.read_excel('Image_Actions.xlsx',sheet_name='Sheet1')
 
 ## Initialise various variables
 st.session_state.emergency_dict=pd.read_excel(book,sheet_name=None)     # Dictionary of emergencies
-st.session_state.action_dict={}                                         # Dictionary of actions
-st.session_state.xylist={}                                              # Dictionary of image coordinates (based on Cockpit image)
-st.session_state.master_list={}                                         # Dictionary of correct Bold Face Actions per emergency
-st.session_state.answer={}                                              # Dictionary of answered options per emergency
-st.session_state.Keys=[]                                                # List of Emergency Titles
+if 'action_dict' not in st.session_state:                               # Dictionary of actions
+    st.session_state.action_dict={} 
+if 'xylist' not in st.session_state:                                          
+    st.session_state.xylist={}                                              # Dictionary of image coordinates (based on Cockpit image)
+if 'master_list' not in st.session_state:  
+    st.session_state.master_list={}                                         # Dictionary of correct Bold Face Actions per emergency
+if 'answer' not in st.session_state:  
+    st.session_state.answer={}                                              # Dictionary of answered options per emergency
+if 'Keys' not in st.session_state:  
+    st.session_state.Keys=[]                                                # List of Emergency Titles
 # Create keys
 Raw_Keys=[i for i in st.session_state.emergency_dict.keys()]
 # Randomise order
@@ -51,7 +56,6 @@ Tabs=st.tabs([str(i+1) for i in range(len(st.session_state.Keys))])
 for j,i in enumerate(st.session_state.num_checklist_list):
     st.session_state.master_list[st.session_state.Keys[j]]=[]
     st.session_state.answer[st.session_state.Keys[j]]=[]
-    selected_image(st.session_state.Keys[j])
     st.markdown(st.session_state.answer)
     layout(Tabs[j],st.session_state.emergency_dict[st.session_state.Keys[j]],st.session_state.Keys[j],j,'Cockpit.png') 
-    
+    selected_image(st.session_state.Keys[j])
