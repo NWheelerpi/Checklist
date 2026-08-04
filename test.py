dfdb79166f114f0550ef52d9121ac68b9219a83f
NWheelerpi <156547753+NWheelerpi@users.noreply.github.com>
2026-08-04 15:40:25 +0100

    n

--- test.py
+++ test.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from streamlit_image_coordinates import streamlit_image_coordinates
-from utils import selected_image,print_string
+from utils import selected_image,print_string,selected
 
 def show_question():
     return
